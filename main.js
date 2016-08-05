e31636520d0d95e85ea6dbf1d437573d9debb040
troy <tjtaill36@gmail.com>
2016-08-04 23:57:33 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    var LanguageManager = brackets.getModule("language/LanguageManager");
+    var CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+    var dot = brackets.getModule("dot");
+    var AppInit	= brackets.getModule("utils/AppInit")
+    
+   
+    function registerDotLanaguage() {
+        
+        dot.register(CodeMirror);
+        
+        CodeMirror.defineMIME('text/x-dot', 'dot');
+
+        LanguageManager.defineLanguage("dot", {
+                name: "DOT",
+                mode: "dot",
+                fileExtensions: ["dot"],
+                blockComment: ["/*", "*/"],
+                lineComment: ["//"]
+            });
+    }
+    
+    AppInit.appReady(function() {
+       registerDotLanaguage(); 
+    });
+   
+});
